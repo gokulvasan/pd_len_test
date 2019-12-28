2b44aedee02f997263de0c50b73d328026234edf
GokulVasan <gokulvas@gmail.com>
2019-12-28 15:35:49 +0100

    Init3

--- pd_len_test.c
+++ pd_len_test.c
@@ -45,7 +45,7 @@ int main(int argc, char** argv)
 		perror("Write is not working :D");
 		exit(-1);
 	}
-	memset(temp, '\0', sizeof(temp));
+	memset(temp, '0x00', sizeof(temp));
 	fgets(temp, 40, fd);
 	printf("Extracted New Value is: %s\n", temp);
 
